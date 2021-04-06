318e49dc949f46be1647447c9945cb4381cdd5c4
CyraX-Joy <77791817+CyraX-Joy@users.noreply.github.com>
2021-04-06 06:13:41 +0530

    Added BMI Calculator into the website

---
+++ bmi-calculator/js/files.js
@@ -0,0 +1,21 @@
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const calculate = document.querySelector("#calculate");
+const yourBMI = document.querySelector("#yourBMI");
+
+calculate.addEventListener("click" , ()=>{
+    //BMI = Body Mass Index
+    //m = Mass Means Weight (KG)
+    //h = Height (CM)
+    //Formula = B = m/h²
+    
+    
+    if(height.value != "" && weight.value != ""){
+        let bmiValue = weight.value / (height.value * height.value) * 10000;
+        yourBMI.innerHTML = `Your BMI Is : <span> ${bmiValue.toFixed(2)} </span>`
+    }
+    else
+        {
+            yourBMI.innerHTML = `Please Enter Correct Value`;
+        }
+});
